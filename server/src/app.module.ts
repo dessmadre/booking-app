4c44fc0fbb7dd769ea51781ad3d3819f67b5dd5d
spacecoupe <iam@joseduarte.io>
2021-10-03 10:36:38 -0700

    Add test db and refactor database module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -7,7 +8,15 @@ import { EventModule } from './event/event.module';
 import { HourModule } from './hour/hour.module';
 
 @Module({
-  imports: [DatabaseModule, DayModule, EventModule, HourModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    DayModule,
+    EventModule,
+    HourModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseService } from './database.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => ({
         uri:
           config.get<string>('NODE_ENV') === 'test'
-            ? ``
-            : `mongodb://${config.get<string>('MONGO_DB')}:27017`,
+            ? config.get<string>('MONGO_URI')
+            : config.get<string>('MONGO_TEST_URI'),
       }),
     }),
   ],
