abc67d857167d58b654cdd1d36ea3f37bd996887
spacecoupe <iam@joseduarte.io>
2021-10-02 19:16:21 -0700

    Add test for event controller

--- server/src/event/event.controller.ts
+++ server/src/event/event.controller.ts
@@ -2,28 +2,29 @@ import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Types } from 'mongoose';
 import { CreateEventDto } from './dto/createEvent.dto';
 import { EventService } from './event.service';
+import { Event } from './schema/event.schema';
 
 @Controller('event')
 export class EventController {
   constructor(private eventService: EventService) {}
 
   @Get('/')
-  async getEvents() {
+  async getEvents(): Promise<Event[]> {
     return this.eventService.getEvents();
   }
 
   @Get('/:id')
-  async getEvent(@Param('id') id: Types.ObjectId) {
+  async getEvent(@Param('id') id: Types.ObjectId): Promise<Event> {
     return this.eventService.getEvent({ id });
   }
 
   @Post('/new')
-  async createEvent(@Body() createEventDto: CreateEventDto) {
+  async createEvent(@Body() createEventDto: CreateEventDto): Promise<Event> {
     return this.eventService.createEvent(createEventDto);
   }
 
   @Delete('/:id')
-  async deleteEvent(@Param('id') id: Types.ObjectId) {
+  async deleteEvent(@Param('id') id: Types.ObjectId): Promise<boolean> {
     return this.eventService.deleteEvent({ id });
   }
 }
--- server/src/event/test/event.controller.spec.ts
+++ server/src/event/test/event.controller.spec.ts
@@ -1,18 +1,98 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateEventDto } from '../dto/createEvent.dto';
 import { EventController } from '../event.controller';
+import { EventService } from '../event.service';
+import { Event } from '../schema/event.schema';
+import { eventStub } from './stubs/event.stub';
+
+jest.mock('../event.service');
 
 describe('EventController', () => {
-  let controller: EventController;
+  let eventController: EventController;
+  let eventService: EventService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EventController],
+      providers: [EventService],
     }).compile();
 
-    controller = module.get<EventController>(EventController);
+    eventController = module.get<EventController>(EventController);
+    eventService = module.get<EventService>(EventService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(eventController).toBeDefined();
+  });
+
+  describe('createEvent', () => {
+    describe('when createEvent is called', () => {
+      let event: Event;
+      let createEventDto: CreateEventDto;
+      beforeEach(async () => {
+        createEventDto = {
+          hour: eventStub().hour,
+          weekday: eventStub().weekday,
+          dateString: eventStub().dateString,
+        };
+
+        event = await eventController.createEvent(createEventDto);
+      });
+      test('then it should call the eventService', () => {
+        expect(eventService.createEvent).toHaveBeenCalledWith({
+          hour: createEventDto.hour,
+          weekday: createEventDto.weekday,
+          dateString: createEventDto.dateString,
+        });
+      });
+      test('then it should return an event', () => {
+        expect(event).toEqual(eventStub());
+      });
+    });
+  });
+
+  describe('getEvents', () => {
+    describe('when getEvents is called', () => {
+      let events: Event[];
+      beforeEach(async () => {
+        events = await eventController.getEvents();
+      });
+      test('then it should call the eventService', () => {
+        expect(eventService.getEvents).toBeCalled();
+      });
+      test('then it should return an array of events', () => {
+        expect(events).toEqual([eventStub()]);
+      });
+    });
+  });
+
+  describe('getEvent', () => {
+    describe('when getEvent is called', () => {
+      let event: Event;
+      beforeEach(async () => {
+        event = await eventController.getEvent(eventStub().id);
+      });
+      test('then it should call the eventService', () => {
+        expect(eventService.getEvent).toBeCalled();
+      });
+      test('then it should return an event', () => {
+        expect(event).toEqual(eventStub());
+      });
+    });
+  });
+
+  describe('createEvent', () => {
+    describe('when createEvent is called', () => {
+      let event: boolean;
+      beforeEach(async () => {
+        event = await eventController.deleteEvent(eventStub().id);
+      });
+      test('then it should call the eventService', () => {
+        expect(eventService.deleteEvent).toBeCalled();
+      });
+      test('then it should return true', () => {
+        expect(event).toEqual(true);
+      });
+    });
   });
 });
