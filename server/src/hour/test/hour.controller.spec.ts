19c448eb3d7666fa31f4ad5a79e3af7006461c01
spacecoupe <iam@joseduarte.io>
2021-10-02 18:26:49 -0700

    Add test for hour controller

--- server/src/hour/test/hour.controller.spec.ts
+++ server/src/hour/test/hour.controller.spec.ts
@@ -1,18 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HourController } from '../hour.controller';
+import { HourService } from '../hour.service';
+import { Hour } from '../schema/hour.schema';
+import { hourStub } from './stubs/hour.stub';
+
+jest.mock('../hour.service');
 
 describe('HourController', () => {
-  let controller: HourController;
+  let hourController: HourController;
+  let hourService: HourService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HourController],
+      providers: [HourService],
     }).compile();
 
-    controller = module.get<HourController>(HourController);
+    hourController = module.get<HourController>(HourController);
+    hourService = module.get<HourService>(HourService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(hourController).toBeDefined();
+  });
+
+  describe('getHours', () => {
+    describe('when getHours is called', () => {
+      let hours: Hour[];
+      beforeEach(async () => {
+        hours = await hourController.getHours();
+      });
+      test('then it should call the hourService', () => {
+        expect(hourService.getHours).toBeCalled();
+      });
+      test('then it should return an array of hours', () => {
+        expect(hours).toEqual([hourStub()]);
+      });
+    });
+  });
+
+  describe('getHour', () => {
+    describe('when getHour gets called', () => {
+      let hour: Hour;
+      beforeEach(async () => {
+        hour = await hourController.getHour(hourStub().id);
+      });
+      test('then it should call the hourService', () => {
+        expect(hourService.getHour).toBeCalled();
+      });
+      test('then it should return an hour', () => {
+        expect(hour).toEqual(hourStub());
+      });
+    });
+  });
+
+  describe('updateHourAvailTrue', () => {
+    describe('when updateHourAvailTrue is called', () => {
+      let hour: Hour;
+      beforeEach(async () => {
+        hour = await hourService.updateHourAvailTrue(hourStub().id);
+      });
+      test('then it should call the hourService', () => {
+        expect(hourService.updateHourAvailTrue).toBeCalled();
+      });
+      test('then it should return an hour', () => {
+        expect(hour).toEqual(hourStub());
+      });
+    });
+  });
+
+  describe('updateHourAvailFalse', () => {
+    describe('when updateHourAvailFalse is called', () => {
+      let hour: Hour;
+      beforeEach(async () => {
+        hour = await hourService.updateHourAvailFalse(hourStub().id);
+      });
+      test('then it should call the hourService', () => {
+        expect(hourService.updateHourAvailFalse).toBeCalled();
+      });
+      test('then it should return an hour', () => {
+        expect(hour).toEqual(hourStub());
+      });
+    });
   });
 });
