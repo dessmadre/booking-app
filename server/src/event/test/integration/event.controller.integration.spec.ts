a4b706d01336d0d16c9f17485841029f8694b040
spacecoupe <iam@joseduarte.io>
2021-10-03 19:07:23 -0700

    Add event integration test

---
+++ server/src/event/test/integration/event.controller.integration.spec.ts
@@ -0,0 +1,95 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { Connection } from 'mongoose';
+import { AppModule } from '../../../app.module';
+import * as request from 'supertest';
+import { DatabaseService } from '../../../database/database.service';
+import { eventStub } from '../stubs/event.stub';
+import { CreateEventDto } from 'src/event/dto/createEvent.dto';
+
+describe('EventController (e2e)', () => {
+  let dbConnection: Connection;
+  let app: INestApplication;
+
+  const apiClient = () => {
+    return request(app.getHttpServer());
+  };
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    dbConnection = module.get<DatabaseService>(DatabaseService).getDbHandle();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    app.close();
+  });
+
+  beforeEach(async () => {
+    await dbConnection.collection('events').deleteMany({});
+  });
+
+  describe('getEvents', () => {
+    it('returns an array or events', async () => {
+      await dbConnection.collection('events').insertOne(eventStub());
+
+      const response = await apiClient().get('/event');
+
+      const eventId = response.body[0]._id;
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual([{ ...eventStub(), _id: eventId }]);
+    });
+  });
+
+  describe('getEvent', () => {
+    it('returns an event', async () => {
+      await dbConnection.collection('events').insertOne(eventStub());
+
+      const events = await apiClient().get('/event');
+
+      const eventId = events.body[0]._id;
+
+      const res = await apiClient().get(`/event/${eventId}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({ ...eventStub(), _id: eventId });
+    });
+  });
+
+  describe('createEvent', () => {
+    it('creates an event', async () => {
+      const createEventDto: CreateEventDto = {
+        hour: eventStub().hour,
+        weekday: eventStub().weekday,
+        dateString: eventStub().dateString,
+      };
+
+      const res = await apiClient().post('/event/new').send(createEventDto);
+
+      const eventId = res.body._id;
+
+      expect(res.status).toBe(201);
+      expect(res.body).toEqual({ ...eventStub(), _id: eventId, __v: 0 });
+    });
+  });
+
+  describe('deleteEvent', () => {
+    it('deletes an event', async () => {
+      await dbConnection.collection('events').insertOne(eventStub());
+
+      const events = await apiClient().get('/event');
+
+      const eventId = events.body[0]._id;
+
+      const res = await apiClient().delete(`/event/${eventId}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({});
+    });
+  });
+});
--- server/src/event/test/stubs/event.stub.ts
+++ server/src/event/test/stubs/event.stub.ts
@@ -2,7 +2,6 @@ import { Event } from '../../schema/event.schema';
 
 export const eventStub = () => {
   return {
-    id: 1,
     hour: '4:00 am',
     weekday: 'Tuesday',
     dateString: 'September 12',
